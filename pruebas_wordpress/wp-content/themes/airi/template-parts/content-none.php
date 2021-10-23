8f8534d87813d647bd18561ea43e5fb6bf2d856e
Salvador Alcover Fuster <salvakanals@gmail.com>
2021-10-23 13:41:03 +0200

    Modificación del tema Airi

---
+++ pruebas_wordpress/wp-content/themes/airi/template-parts/content-none.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Template part for displaying a message that posts cannot be found
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Airi
+ */
+
+?>
+
+<section class="no-results not-found">
+	<header class="page-header">
+		<h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'airi' ); ?></h1>
+	</header><!-- .page-header -->
+
+	<div class="page-content">
+		<?php
+		if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
+
+			<p><?php
+				printf(
+					wp_kses(
+						/* translators: 1: link to WP admin new post page. */
+						__( 'Preparado para publicar tu primer post? <a href="%1$s">Get started here</a>.', 'airi_mod' ),
+						array(
+							'a' => array(
+								'href' => array(),
+							),
+						)
+					),
+					esc_url( admin_url( 'post-new.php' ) )
+				);
+			?></p>
+
+		<?php elseif ( is_search() ) : ?>
+
+			<p><?php esc_html_e( 'Lo siento, no se han encontrado resultado. Pruebe a buscar con otras palabras.', 'airi_mod' ); ?></p>
+			<?php
+				get_search_form();
+
+		else : ?>
+
+			<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'airi_mod' ); ?></p>
+			<?php
+				get_search_form();
+
+		endif; ?>
+	</div><!-- .page-content -->
+</section><!-- .no-results -->
